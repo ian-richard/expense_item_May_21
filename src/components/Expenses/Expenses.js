434f90d17f9ea86d9b98faadd6cfbfc225363519
Ian F <ian.fiddeman@gmail.com>
2021-05-27 20:32:00 +0100

    list added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,8 +3,10 @@ import React, { useState } from 'react';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
-import ExpenseItem from './ExpenseItem';
+
 import './ExpensesFilter.css';
+import ExpensesList from './ExpensesList';
+
 
 
 const Expenses = (props) => {
@@ -19,32 +21,21 @@ const Expenses = (props) => {
 
     const filteredExpenses = props.items.filter(expenses => {
       return expenses.date.getFullYear().toString() === filteredYear;
-    })
-
-    let expensesContent = <p> No expenses found.</p>;
-
-    if(filteredExpenses.length > 0){
-      expensesContent = filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}  
-        />
-        ))
-        }
+    });
     
 
     return (
-      <div>
-        <Card className="expenses">
-          <ExpensesFilter
-            selected={filteredYear}
-            onChangeFilter={filterChangeHandler}
-          />
-          {expensesContent}
-        </Card>
-      </div>
+      <li>
+        <div>
+          <Card className="expenses">
+            <ExpensesFilter
+              selected={filteredYear}
+              onChangeFilter={filterChangeHandler}
+            />
+            <ExpensesList items={filteredExpenses}/>
+          </Card>
+        </div>
+      </li>
     );
     }
 
